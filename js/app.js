e291fd99fbaa1f2251675016d8d11a559f3f9f34
roman-yagodin <roman.yagodin@gmail.com>
2024-02-08 15:46:18 +0300

    Move player names and themes to separate files

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 import { notes } from "./data.js";
+import { playerNames } from "./playerNames.js";
+import { colorTheme, bowTheme } from "./themes.js";
 import { DEBUG } from "./debug.js";
 
 const EOL = "\n\r";
@@ -10,53 +12,6 @@ const _2s = _1s * 2;
 const _3s = _1s * 3;
 const _4s = _1s * 4;
 const _5s = _1s * 5;
-
-const colorTheme = {
-    foreground: '#F8F8F8',
-    background: '#2D2E2C',
-    selection: '#5DA5D533',
-    // black: '#1E1E1D', 
-    // brightBlack: '#262625',
-    black: '#CE8500', // reuse black for orange (1)
-    brightBlack: '#FFA500',
-    red: '#CE5C5C',
-    brightRed: '#FF7272',
-    green: '#5BCC5B',
-    brightGreen: '#72FF72',
-    yellow: '#CCCC5B',
-    brightYellow: '#FFFF72',
-    blue: '#5D5DD3',
-    brightBlue: '#7279FF',
-    magenta: '#BC5ED1',
-    brightMagenta: '#E572FF',
-    cyan: '#5DA5D5',
-    brightCyan: '#72F0FF',
-    white: '#F8F8F8',
-    brightWhite: '#FFFFFF'
-};
-
-const bowTheme = {
-    foreground: '#111111',
-    background: '#FFFFFF',
-    selection: '#CCCCCC',
-    black: '#222222', 
-    brightBlack: '#333333',
-    red: '#222222',
-    brightRed: '#333333',
-    green: '#222222',
-    brightGreen: '#333333',
-    yellow: '#222222',
-    brightYellow: '#333333',
-    blue: '#222222',
-    brightBlue: '#333333',
-    magenta: '#222222',
-    brightMagenta: '#333333',
-    cyan: '#222222',
-    brightCyan: '#333333',
-    white: '#DDDDDD',
-    brightWhite: '#EEEEEE',
-    cursor: '#111111'
-};
 
 const styles = {
     default: "\x1b[0m",
@@ -74,26 +29,6 @@ const styles = {
     cyan: "\x1b[36m",
     white: "\x1b[37m"
 };
-
-const playerNames = [
-    "@",
-    "player",
-    "user",
-    "operator",
-    "human",
-    "humanoid",
-    "creature",
-    "creator",
-    "reader",
-    "dear",
-    "darling",
-    "precious",
-    "friend",
-    "child",
-    "adventurer",
-    "traveler",
-    "wanderer",
-];
 
 class Game {
     constructor() {
---
+++ js/themes.js
@@ -0,0 +1,46 @@
+export const colorTheme = {
+    foreground: '#F8F8F8',
+    background: '#2D2E2C',
+    selection: '#5DA5D533',
+    // black: '#1E1E1D', 
+    // brightBlack: '#262625',
+    black: '#CE8500', // reuse black for orange (1)
+    brightBlack: '#FFA500',
+    red: '#CE5C5C',
+    brightRed: '#FF7272',
+    green: '#5BCC5B',
+    brightGreen: '#72FF72',
+    yellow: '#CCCC5B',
+    brightYellow: '#FFFF72',
+    blue: '#5D5DD3',
+    brightBlue: '#7279FF',
+    magenta: '#BC5ED1',
+    brightMagenta: '#E572FF',
+    cyan: '#5DA5D5',
+    brightCyan: '#72F0FF',
+    white: '#F8F8F8',
+    brightWhite: '#FFFFFF'
+};
+
+export const bowTheme = {
+    foreground: '#111111',
+    background: '#FFFFFF',
+    selection: '#CCCCCC',
+    black: '#222222', 
+    brightBlack: '#333333',
+    red: '#222222',
+    brightRed: '#333333',
+    green: '#222222',
+    brightGreen: '#333333',
+    yellow: '#222222',
+    brightYellow: '#333333',
+    blue: '#222222',
+    brightBlue: '#333333',
+    magenta: '#222222',
+    brightMagenta: '#333333',
+    cyan: '#222222',
+    brightCyan: '#333333',
+    white: '#DDDDDD',
+    brightWhite: '#EEEEEE',
+    cursor: '#111111'
+};
